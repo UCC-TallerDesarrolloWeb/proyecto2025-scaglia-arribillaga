0d2774c967b4a8f7f2516228c6c20c82021b7099
Facu Arribilaga <2411006@ucc.edu.ar>
2025-10-05 19:58:39 -0300

    Detalles responsive del detalle de cada pokemon

--- script.js
+++ script.js
@@ -99,17 +99,17 @@ document.addEventListener("DOMContentLoaded", () => {
     detalle.querySelector("#detallePeso").textContent = "Peso: " + peso;
     detalle.querySelector("#detalleDescripcion").textContent = descripcion;
     detalle.querySelector("#statHP").textContent = hp;
-    detalle.querySelector("#barraHP").innerHTML = "<div class='barraHP' style='width: calc(" + hp + "% / 1.2);'></div>";
+    detalle.querySelector("#barraHP").innerHTML = "<div class='barraHP' style='width: calc((" + hp + "/250)*100%);'></div>";
     detalle.querySelector("#statAtk").textContent = atk;
-    detalle.querySelector("#barraAtk").innerHTML = "<div class='barraAtk' style='width: calc(" + atk + "% / 1.2);'></div>";
+    detalle.querySelector("#barraAtk").innerHTML = "<div class='barraAtk' style='width: calc((" + atk + "/250)*100%);'></div>";
     detalle.querySelector("#statDef").textContent = def;
-    detalle.querySelector("#barraDef").innerHTML = "<div class='barraDef' style='width: calc(" + def + "% / 1.2);'></div>";
+    detalle.querySelector("#barraDef").innerHTML = "<div class='barraDef' style='width: calc((" + def + "/250)*100%);'></div>";
     detalle.querySelector("#statAtkEsp").textContent = atkesp;
-    detalle.querySelector("#barraAtkEsp").innerHTML = "<div class='barraAtkEsp' style='width: calc(" + atkesp + "% / 1.2);'></div>";
+    detalle.querySelector("#barraAtkEsp").innerHTML = "<div class='barraAtkEsp' style='width: calc((" + atkesp + "/250)*100%);'></div>";
     detalle.querySelector("#statDefEsp").textContent = defesp;
-    detalle.querySelector("#barraDefEsp").innerHTML = "<div class='barraDefEsp' style='width: calc(" + defesp + "% / 1.2);'></div>";
+    detalle.querySelector("#barraDefEsp").innerHTML = "<div class='barraDefEsp' style='width: calc((" + defesp + "/250)*100%);'></div>";
     detalle.querySelector("#statVel").textContent = vel;
-    detalle.querySelector("#barraVel").innerHTML = "<div class='barraVel' style='width: calc(" + vel + "% / 1.2);'></div>";
+    detalle.querySelector("#barraVel").innerHTML = "<div class='barraVel' style='width: calc((" + vel + "/250)*100%);'></div>";
 
 
     const evoContainer = detalle.querySelector("#detalleEvoluciones");
