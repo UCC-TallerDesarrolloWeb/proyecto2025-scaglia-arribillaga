ee89546902613feb58ddf173b67384dacc8114d1
unknown <Jis12102005@gmail.com>
2025-11-06 14:04:22 -0300

    modificaciones

--- frontend/proyecto2025-scaglia-arribillaga/vite.config.js
+++ frontend/proyecto2025-scaglia-arribillaga/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-// https://vite.dev/config/
+// ✅ Emulación necesaria: Vite NO tiene __dirname por defecto
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@data": path.resolve(__dirname, "src/Data"),
+    },
+  },
+});
