c9540b379519c660d164903883128a5f9b93ce4b
unknown <Jis12102005@gmail.com>
2026-02-03 16:25:06 -0300

    se elimino imports innecesarios

--- frontend/proyecto2025-scaglia-arribillaga/src/main.jsx
+++ frontend/proyecto2025-scaglia-arribillaga/src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
@@ -9,9 +8,9 @@ import "@styles/layout.scss";
 import "@styles/intro.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-  </React.StrictMode>
+
 );
